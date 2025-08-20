c0b6d4b060e12048e581e84b2b9547cbf7664644
theselegadev <matheus.seleghin7@gmail.com>
2025-08-20 19:43:21 -0300

    ong criando conta

--- App/js/request.js
+++ App/js/request.js
@@ -12,6 +12,20 @@ export async function requestCreateAccount(body){
     return await response.json()
 }
 
+// função que faz requisição no método post na rota /ong da api para criar ong
+export async function requestCreateOng(body){
+    const endpoint = "http://localhost/solidarize/Api/ong"
+    const response = await fetch(endpoint,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: body
+    }).catch(err=>console.error("Erro: ", err))
+
+    return await response.json()
+}
+
 // função que faz a requisição no método get na rota /user da api bara buscar dados do user
 export async function requestGetUser(id){
     const endpoint = `http://localhost/solidarize/Api/user/${id}`
