6937d749b88919508a69fb6e93ba556485ccc947
theselegadev <matheus.seleghin7@gmail.com>
2025-09-08 16:03:13 -0300

    recomendando perfis com paginação para o usuário (falta testar mais)

--- App/js/home.js
+++ App/js/home.js
@@ -1,8 +1,28 @@
-import { showUserData, logout } from "./module.js";
+import { showUserData, logout,renderPagination, renderProfiles, renderAlert } from "./module.js";
+import { requestGetProfileRecommended } from "./request.js";
 
 const btnLogout = document.querySelector("#logout")
 
-document.addEventListener('DOMContentLoaded', showUserData())
+document.addEventListener('DOMContentLoaded', async ()=>{
+  const id = await showUserData()
+
+  async function loadPage(page){
+    const response = await requestGetProfileRecommended(id,page)
+    const currentPage = response.data.page
+    const totalPage = response.data.total_pages
+    
+    if(response.status === 'success'){
+      renderProfiles(response.data.profiles)
+      renderPagination(totalPage,currentPage,loadPage)
+    }else{
+      renderAlert(response.message)
+    }
+
+
+  }
+  
+  await loadPage(1)
+})
 
 btnLogout.addEventListener('click', async ()=>{
     await logout()
