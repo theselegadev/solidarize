2767f4dc74c8b986534ad145795967cc6f8d01e3
theselegadev <matheus.seleghin7@gmail.com>
2025-09-15 15:55:01 -0300

    ongs definem se precisam de voluntários ou não

--- App/js/homeOng.js
+++ App/js/homeOng.js
@@ -1,6 +1,15 @@
-import { logout } from "./module.js";
+import { logout, handleNeedVolunteer } from "./module.js";
+import { requestDataUser } from "./request.js";
 
 const btnLogout = document.querySelector("#logout")
+
+document.addEventListener("DOMContentLoaded", async ()=>{
+    const responseDataUser = await requestDataUser()
+    const id = responseDataUser.data.user_id
+
+    await handleNeedVolunteer(id)
+
+})
 
 btnLogout.addEventListener('click', async ()=>{
     await logout()
